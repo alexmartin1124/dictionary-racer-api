b11003d861c86a77cac8ddeaf8d13aebf78eb7e5
alexseth1124 <alexseth1124@gmail.com>
2016-06-17 17:07:51 -0400

    most of this working

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module DictionaryRacerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    config.autoload_paths << Rails.root.join("lib")
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
       resources :entries
     end
   end
+  post '/login', to: 'sessions#create'
 end
