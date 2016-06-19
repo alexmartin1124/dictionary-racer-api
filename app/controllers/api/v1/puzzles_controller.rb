e7a19db5af11c0bf6919226b33d9a78d35e9b90c
alexseth1124 <alexseth1124@gmail.com>
2016-06-19 16:17:43 -0400

    people can create puzzles

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -4,6 +4,33 @@ module Api
       def index
         render json: Puzzle.includes(:entries, :users), include: ["entries", "users"]
       end
+
+      def create
+        start_word = puzzle_params["start-word"]
+        end_word = puzzle_params["end-word"]
+        entries = [start_word, end_word]
+        entries.map! do |word|
+          if Entry.find_by(word: word)
+            Entry.find_by(word: word)
+          else
+            EntryCreator.new(word).create_entry
+          end
+        end
+        puzzle = Puzzle.new(start_word: start_word, end_word: end_word)
+        entries.each{|entry| puzzle.entries << entry}
+        puzzle.users << current_user
+        if puzzle.save
+          render json: puzzle
+        else
+          render json: {error: "Invalid puzzle."}
+        end
+      end
+
+    private
+
+      def puzzle_params
+        params[:data].require(:attributes).permit("start-word", "end-word")
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :puzzles
+      post '/puzzles', to: 'puzzles#create'
       resources :users
       get '/users/current-user', to: 'current_user#show'
       resources :entries
     end
   end
   post '/login', to: 'sessions#create'
+
 end
