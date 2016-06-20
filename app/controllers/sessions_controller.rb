b7303f212a9d388071219110233f2d8d29b2db07
alexseth1124 <alexseth1124@gmail.com>
2016-06-20 14:11:45 -0400

    took class auth out of seshes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,23 +21,3 @@ class SessionsController < ApplicationController
   end
 
 end
-
-class Auth
-
-  ALGORITHM = 'HS256'
-
-  def self.issue(payload)
-    JWT.encode(payload, auth_secret, ALGORITHM)
-  end
-
-  def self.decode(token)
-    JWT.decode(token, auth_secret, false, {algorithm: ALGORITHM}).first
-  end
-
-private
-
-  def self.auth_secret
-    ENV["AUTH_SECRET"]
-  end
-
-end
