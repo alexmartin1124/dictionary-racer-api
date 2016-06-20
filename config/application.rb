bd975314ff971b60f616d3a5efeaa10340aa4898
alexseth1124 <alexseth1124@gmail.com>
2016-06-20 13:27:19 -0400

    switched order of two things in config/application

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module DictionaryRacerApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
     config.autoload_paths << Rails.root.join("lib")
+    config.api_only = true
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
