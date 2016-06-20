f29a2239de0789571065239ab16e6c0f32b1ea04
alexseth1124 <alexseth1124@gmail.com>
2016-06-20 10:50:23 -0400

    mak9ng seed file work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-nouns = File.read("nounlist.txt").split("\n")
-nouns.each {|noun| EntryCreator.new(noun).create_entry}
+# nouns = File.read("nounlist.txt").split("\n")
+# nouns.each {|noun| EntryCreator.new(noun).create_entry}
 
 
 alex = User.create(username: "alex", email: "alex@dictionaryracer.com", password: "password", password_confirmation: "password")
